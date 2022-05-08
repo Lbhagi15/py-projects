1edb6125d6c420b12cfbf4bd974263945e1abac9
Lbhagi15 <lakshmibhargavij@gmail.com>
2022-05-08 22:05:08 +0530

    lab

---
+++ if.py
@@ -0,0 +1,8 @@
+# find out the four punctuation's are present in the given string
+# enter the string
+
+str1= 'bhagi! very,innocent girl:.'
+
+if ',' in str1:
+    print("given tring has .")
+if ''!
